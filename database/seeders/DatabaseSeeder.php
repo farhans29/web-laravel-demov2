3a87a2797add87035008a3dbc3a514f2edbaafc3
M Farhan Syihab <m.farhanshihab11@gmail.com>
2025-02-24 17:04:48 +0700

    add:migration table m_warehouse and data seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-
         $this->call([
             DashboardTableSeeder::class,
+            MWarehouseSeeder::class,
         ]);
     }
 }
